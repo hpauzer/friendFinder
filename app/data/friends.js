3dc6fbde326106639bfa1a31087a7d7f5b0ecfc6
hpauzer <heatherpauzer@Heathers-MBP-2.attlocal.net>
2017-10-27 18:24:37 -0500

    file structure

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var friendsArray = [{
+        name: "Brenda",
+        link: "https://vignette.wikia.nocookie.net/90210/images/4/4e/208brendawalsh.jpeg/revision/latest/scale-to-width-down/310?cb=20170406151502",
+        responses: [5, 5, 5, 4, 4, 4, 5, 5, 4, 4]
+    },
+
+    {
+        name: "Kelly",
+        link: "https://vignette.wikia.nocookie.net/90210/images/e/e9/K1.jpg/revision/latest/scale-to-width-down/310?cb=20160806230807",
+        responses: [4, 4, 4, 4, 4, 4, 4, 4, 4, 3]
+    },
+
+    {
+        name: "Donna",
+        link: "https://rejectedbookplots.files.wordpress.com/2011/10/donna_2.jpg",
+        responses: [3, 3, 3, 3, 3, 3, 3, 3, 3, 2]
+    },
+
+    {
+        name: "Andrea",
+        link: "http://farm4.static.flickr.com/3051/2712384797_8841be7496.jpg",
+        responses: [2, 3, 2, 1, 1, 2, 1, 2, 2, 1]
+    },
+];
+
+module.exports = friendsArray;
